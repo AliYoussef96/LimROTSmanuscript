ce15a1d0259f640bad5c12f21d3008979d074ab4
Ali Youssef <aliali.mostafa99@gmail.com>
2025-05-21 09:51:03 +0300

    Update spectronaut.run.r

--- DIA/Case study 1/spectronaut.run.r
+++ DIA/Case study 1/spectronaut.run.r
@@ -1,300 +1,165 @@
+
+# Load required packages
 library(stringr)
 library(SummarizedExperiment)
 library(LimROTS)
 library(ROTS)
 library(BiocParallel)
 library(rrcovNA)
-library(samr)
+library(imputeLCMD)
 library(limma)
+library(samr)
 
-#### #### #### #### #### #### #### #### #### 
-#### #### #### #### #### #### #### #### #### 
-#### LimROTS, ROTS
-#### #### #### #### #### #### #### #### #### 
-#### #### #### #### #### #### #### #### #### 
-
-all.exp <- list.files("../Spectronaut/")
-all.exp <- str_split_fixed(all.exp, "_DIA_" , 2)[,1]
+# List all experiments from the Spectronaut directory
+all.exp <- list.files("Spectronaut/")
+all.exp <- str_split_fixed(all.exp, "_LFQ_Spectronaut_" , 2)[,1]
 all.exp <- unique(all.exp)
 
-data.file <- list.files("data/", pattern = "PXD")
-data.info <- read.csv("data/data.info.csv")
-data.info$Dataset <- str_split_fixed(data.info$Dataset , fixed("_"), 2)[,1]
+# Set seed for reproducibility
+set.seed(1597, sample.kind = "default", kind = "default")
 
-
-set.seed(1597, sample.kind = "default" , kind = "default")
-
-
+# Loop through each experiment folder
 for(i.exp in all.exp){
-    print(i.exp)
-    
-    exp.mar <- read.csv(paste0("../Spectronaut/", i.exp , "_DIA_spt_dlfq.tsv"), sep = "\t")
-    design <- read.csv(paste0("../Spectronaut/", i.exp , "_DIA_spt_design.tsv"), sep = "\t")
-    Contrasts <- combn(design$condition, 2, simplify = F)
-    Contrasts <- unique(sapply(Contrasts, function(x) paste(sort(x), collapse = "")))
-    Contrasts <- Contrasts[!sapply(Contrasts, function(x) length(unique(strsplit(x, "")[[1]])) == 1)]
-    row.names(exp.mar) <- exp.mar$Protein
-    exp.mar$Protein <- NULL
-    exp.mar$Organism <- NULL
-    design$sample_name <- str_remove(design$sample_name, fixed("_"))
-    colnames(exp.mar) <- str_remove(colnames(exp.mar), fixed("_"))
-    
-    exp.mar <- exp.mar[,colnames(exp.mar) %in% design$sample_name]
-    design <- design[design$sample_name %in% colnames(exp.mar),]
-    exp.mar <- exp.mar[,design$sample_name]
-    
+  message("Processing experiment: ", i.exp)
+  
+  # Load intensity and design tables
+  exp.mar <- read.csv(file.path("Spectronaut", paste0(i.exp, "_LFQ_Spectronaut_dlfq_pro_intensity.tsv")), sep = "\t")
+  design <- read.csv(file.path("Spectronaut", paste0(i.exp, "_LFQ_Spectronaut_design.tsv")), sep = "\t")
 
-    for(i.Contrasts in Contrasts){
-        print(i.Contrasts)
-        
-        i.Contrasts1 <- str_split(i.Contrasts, "")[[1]][1]
-        i.Contrasts2 <- str_split(i.Contrasts, "")[[1]][2]
-        
-        
-        select.col.Contrasts1 <- nrow(design[design$condition %in% c(i.Contrasts1),])
-        select.col.Contrasts2 <- nrow(design[design$condition %in% c(i.Contrasts2),])
-        
-        design.temp <- design[design$condition %in% c(i.Contrasts1, i.Contrasts2),]
-        
-        df.temp <- exp.mar[,colnames(exp.mar) %in% design.temp$sample_name]
-        
-        design.temp <- design.temp[order(design.temp$condition),]
-        
-        df.temp <- df.temp[,design.temp$sample_name]
-        
+  # Create condition contrasts (e.g., AB, AC)
+  Contrasts <- combn(design$condition, 2, simplify = FALSE)
+  Contrasts <- unique(sapply(Contrasts, function(x) paste(sort(x), collapse = "")))
+  Contrasts <- Contrasts[!sapply(Contrasts, function(x) length(unique(strsplit(x, "")[[1]])) == 1)]
 
-        if(select.col.Contrasts1 > 1 & select.col.Contrasts2 > 1){
-       if(any(is.na(df.temp))){
-           df.temp <- log2(df.temp+1)
-           df.temp <- impSeq(df.temp)
-           df.temp <- data.frame(df.temp, check.names = F, check.rows = F)
-       }else{
-           df.temp[df.temp == 0 ] <- NA
-           df.temp <- log2(df.temp+1)
-           df.temp <- impSeq(df.temp)
-           df.temp <- data.frame(df.temp, check.names = F, check.rows = F)
-        }
+  # Preprocess expression data
+  row.names(exp.mar) <- exp.mar$Protein
+  exp.mar$Protein <- NULL
+  exp.mar$Organism <- NULL
+  design$sample_name <- str_remove(design$sample_name, fixed("_"))
+  colnames(exp.mar) <- str_remove(colnames(exp.mar), fixed("_"))
 
-        
+  exp.mar <- exp.mar[, colnames(exp.mar) %in% design$sample_name]
+  design <- design[design$sample_name %in% colnames(exp.mar),]
+  exp.mar <- exp.mar[, design$sample_name]
 
-        sample_info <- data.frame(
-            sample_id = colnames(df.temp),
-            group = c(rep(i.Contrasts1, select.col.Contrasts1),
-                      rep(i.Contrasts2, select.col.Contrasts2)))
-            
-        rownames(sample_info) <- row.names(sample_info$sample_id)
-        sample_info$group <- as.factor(sample_info$group)
-        
-        feature_info <- data.frame(
-            protein_id = row.names(df.temp)
-        )
-        rownames(feature_info) <- feature_info$protein_id
-        
-        
-        # Create the SummarizedExperiment object
-        se <- SummarizedExperiment(
-            assays = list(protein.exp = df.temp),
-            colData = sample_info,
-            rowData = feature_info 
-        )
-        
-        # Set metadata and formula for LimROTS analysis
-        meta.info <- c("group")
-        niter <- 500 # Number of bootstrap samples
-        K <- nrow(feature_info) / 2 # Set the value for K based on the data size
-        K <- floor(K)
-        group.name <- "group"
-        formula.str <- "~ 0 + group" # Formula for group comparison
-        
-        
-        # Run LimROTS analysis with trend and robust settings enabled
-        se <- LimROTS(
-            x = se,
-            niter = niter, K = K, meta.info = meta.info,
-            BPPARAM  = SnowParam(10, progressbar = T), group.name = group.name,
-            formula.str = formula.str, trend = TRUE,
-            robust = TRUE, permutating.group = FALSE
-        )
-        
-        saveRDS(se, paste0("spectronaut_resutls/", "LimROTS_" , i.exp , "_" , i.Contrasts, ".rds"))
-     
-        remove(se)
-        gc()
-        
-        # run ROTS
-        rots_results <- ROTS(data = df.temp , groups = as.numeric(sample_info$group), B = niter, K = K, 
-                             seed = 1597, progress = TRUE, verbose = TRUE)
-        
-        saveRDS(rots_results, paste0("spectronaut_resutls/", "ROTS_" , i.exp , "_" , i.Contrasts, ".rds"))
-        
-        remove(rots_results)
-        gc()
-        
-        
-        }
+  # Loop through each contrast
+  for(i.Contrasts in Contrasts){
+    message("Running contrast: ", i.Contrasts)
+
+    # Define groups
+    groups <- str_split(i.Contrasts, "")[[1]]
+    i.Contrasts1 <- groups[1]
+    i.Contrasts2 <- groups[2]
+
+    select.col.Contrasts1 <- sum(design$condition == i.Contrasts1)
+    select.col.Contrasts2 <- sum(design$condition == i.Contrasts2)
+
+    # Require at least two samples per group
+    if(select.col.Contrasts1 > 1 & select.col.Contrasts2 > 1){
+      design.temp <- design[design$condition %in% groups,]
+      df.temp <- exp.mar[, design.temp$sample_name]
+
+      # Log2 transform and impute missing
+      df.temp[df.temp == 0] <- NA
+      df.temp <- log2(df.temp + 1)
+      df.temp <- impSeq(df.temp)
+      df.temp <- data.frame(df.temp, check.names = FALSE)
+
+      # Create sample and feature info
+      sample_info <- data.frame(
+        sample_id = colnames(df.temp),
+        group = factor(c(rep(i.Contrasts1, select.col.Contrasts1),
+                         rep(i.Contrasts2, select.col.Contrasts2)))
+      )
+      rownames(sample_info) <- sample_info$sample_id
+
+      feature_info <- data.frame(
+        protein_id = rownames(df.temp)
+      )
+      rownames(feature_info) <- feature_info$protein_id
+
+      # Create SummarizedExperiment object
+      se <- SummarizedExperiment(
+        assays = list(protein.exp = df.temp),
+        colData = sample_info,
+        rowData = feature_info 
+      )
+
+      # Run LimROTS
+      se <- LimROTS(
+        x = se,
+        niter = 500,
+        K = floor(nrow(df.temp) / 2),
+        meta.info = "group",
+        BPPARAM = SnowParam(5, progressbar = TRUE),
+        group.name = "group",
+        formula.str = "~ 0 + group",
+        trend = TRUE,
+        robust = TRUE
+      )
+      saveRDS(se, file.path("Spectronaut_results", paste0("LimROTS_", i.exp, "_", i.Contrasts, ".rds")))
+      rm(se); gc()
+
+      # ROTS
+      rots_results <- ROTS(
+        data = df.temp,
+        groups = as.numeric(sample_info$group),
+        B = 500,
+        K = floor(nrow(df.temp) / 2),
+        seed = 1597,
+        progress = TRUE,
+        verbose = TRUE
+      )
+      saveRDS(rots_results, file.path("Spectronaut_results", paste0("ROTS_", i.exp, "_", i.Contrasts, ".rds")))
+      rm(rots_results); gc()
+
+      # Limma
+      design.model <- model.matrix(~0 + group, data = sample_info)
+      fit <- lmFit(df.temp, design = design.model)
+      contrast <- makeContrasts(contrasts = paste0("group", i.Contrasts1, "-group", i.Contrasts2), levels = design.model)
+      fit <- eBayes(contrasts.fit(fit, contrast), trend = TRUE, robust = TRUE)
+      limma.res <- topTable(fit, adjust = "BH", sort.by = "logFC", n = Inf)
+      saveRDS(limma.res, file.path("Spectronaut_results", paste0("Limma_", i.exp, "_", i.Contrasts, ".rds")))
+      rm(limma.res); gc()
+
+      # SAM
+      sam.data <- list(
+        x = as.matrix(df.temp),
+        y = as.numeric(sample_info$group),
+        geneid = feature_info$protein_id,
+        genenames = feature_info$protein_id,
+        logged2 = TRUE
+      )
+      sam.obj <- samr(sam.data, resp.type = "Two class unpaired", nperms = 100)
+      logFC <- log2(sam.obj$foldchange)
+      pval <- samr.pvalues.from.perms(sam.obj$tt, sam.obj$ttstar)
+      adj.p <- p.adjust(pval, method = "BH")
+      SAM.res <- cbind(logFC, pvalue = pval, adj.pvalue = adj.p)
+      saveRDS(SAM.res, file.path("Spectronaut_results", paste0("SAM_", i.exp, "_", i.Contrasts, ".rds")))
+      rm(SAM.res); gc()
+
+      # t-test
+      ttest.res <- t(sapply(1:nrow(df.temp), function(i){
+        test <- t.test(as.numeric(df.temp[i,]) ~ sample_info$group)
+        fc <- mean(df.temp[i, sample_info$group == i.Contrasts1]) - mean(df.temp[i, sample_info$group == i.Contrasts2])
+        c(pval = test$p.value, fc = fc)
+      }))
+      ttest.res <- as.data.frame(ttest.res)
+      ttest.res$adj.pvalue <- p.adjust(ttest.res$pval, method = "BH")
+      rownames(ttest.res) <- rownames(df.temp)
+      saveRDS(ttest.res, file.path("Spectronaut_results", paste0("ttest_", i.exp, "_", i.Contrasts, ".rds")))
+      rm(ttest.res); gc()
+
+      # ANOVA
+      fit <- lmFit(df.temp, design = model.matrix(~0 + group, data = sample_info))
+      ord.t <- fit$coefficients[, 1] / fit$sigma / fit$stdev.unscaled[, 1]
+      ord.p <- 2 * pt(abs(ord.t), df = fit$df.residual, lower.tail = FALSE)
+      ord.q <- p.adjust(ord.p, method = "BH")
+      logFC <- fit$coefficients[, 2] - fit$coefficients[, 1]
+      anova.res <- data.frame(logFC = logFC, t = ord.t, P.Value = ord.p, adj.P.Val = ord.q)
+      rownames(anova.res) <- rownames(df.temp)
+      saveRDS(anova.res, file.path("Spectronaut_results", paste0("ANOVA_", i.exp, "_", i.Contrasts, ".rds")))
+      rm(anova.res); gc()
     }
+  }
 }
-
-#### #### #### #### #### #### #### #### #### 
-#### #### #### #### #### #### #### #### #### 
-#### Limma, SAM, ANOVA and t-test
-#### #### #### #### #### #### #### #### #### 
-#### #### #### #### #### #### #### #### ####
-
-
-
-for(i.exp in all.exp){
-    print(i.exp)
-    
-    exp.mar <- read.csv(paste0("../Spectronaut/", i.exp , "_DIA_spt_dlfq.tsv"), sep = "\t")
-    design <- read.csv(paste0("../Spectronaut/", i.exp , "_DIA_spt_design.tsv"), sep = "\t")
-    Contrasts <- combn(design$condition, 2, simplify = F)
-    Contrasts <- unique(sapply(Contrasts, function(x) paste(sort(x), collapse = "")))
-    Contrasts <- Contrasts[!sapply(Contrasts, function(x) length(unique(strsplit(x, "")[[1]])) == 1)]
-    row.names(exp.mar) <- exp.mar$Protein
-    exp.mar$Protein <- NULL
-    exp.mar$Organism <- NULL
-    design$sample_name <- str_remove(design$sample_name, fixed("_"))
-    colnames(exp.mar) <- str_remove(colnames(exp.mar), fixed("_"))
-    
-    exp.mar <- exp.mar[,colnames(exp.mar) %in% design$sample_name]
-    design <- design[design$sample_name %in% colnames(exp.mar),]
-    
-    exp.mar <- exp.mar[,design$sample_name]
-    
-    
-    for(i.Contrasts in Contrasts){
-        print(i.Contrasts)
-        
-        i.Contrasts1 <- str_split(i.Contrasts, "")[[1]][1]
-        i.Contrasts2 <- str_split(i.Contrasts, "")[[1]][2]
-        
-        
-        select.col.Contrasts1 <- nrow(design[design$condition %in% c(i.Contrasts1),])
-        select.col.Contrasts2 <- nrow(design[design$condition %in% c(i.Contrasts2),])
-        
-        design.temp <- design[design$condition %in% c(i.Contrasts1, i.Contrasts2),]
-        
-        design.temp <- design.temp[order(design.temp$condition),]
-        
-        df.temp <- exp.mar[,colnames(exp.mar) %in% design.temp$sample_name]
-        
-        df.temp <- df.temp[,design.temp$sample_name]
-        
-        if(select.col.Contrasts1 > 1 & select.col.Contrasts2 > 1){
-        if(any(is.na(df.temp))){
-            df.temp <- log2(df.temp+1)
-            df.temp <- impSeq(df.temp)
-            df.temp <- data.frame(df.temp, check.names = F, check.rows = F)
-        }else{
-            df.temp[df.temp == 0 ] <- NA
-            df.temp <- log2(df.temp+1)
-            df.temp <- impSeq(df.temp)
-            df.temp <- data.frame(df.temp, check.names = F, check.rows = F)
-        }
-        
-        
-        
-        sample_info <- data.frame(
-            sample_id = colnames(df.temp),
-            group = c(rep(i.Contrasts1, select.col.Contrasts1),
-                      rep(i.Contrasts2, select.col.Contrasts2)))
-        
-        rownames(sample_info) <- row.names(sample_info$sample_id)
-        sample_info$group <- as.factor(sample_info$group)
-        
-        feature_info <- data.frame(
-            protein_id = row.names(df.temp)
-        )
-        rownames(feature_info) <- feature_info$protein_id
-        
-        
-        # Create the SummarizedExperiment object
-        se <- SummarizedExperiment(
-            assays = list(protein.exp = df.temp),
-            colData = sample_info,
-            rowData = feature_info 
-        )
-        
-        
-        # run limma
-        
-        design.model = model.matrix(~0+group , data = sample_info) 
-        
-        fit1 = lmFit(df.temp, design = design.model)
-        cont <- makeContrasts(contrasts =  paste0( "group", i.Contrasts1 , "-" , "group" , i.Contrasts2), levels = design.model)
-        fit2 = contrasts.fit(fit1,contrasts = cont)
-        fit3 <- eBayes(fit2, trend = T, robust = T)
-        limma.results = topTable(fit3, adjust="BH", sort.by = 'logFC', n=Inf)
-        
-        saveRDS(limma.results, paste0("spectronaut_resutls/", "Limma_" , i.exp , "_" , i.Contrasts, ".rds"))
-        remove(limma.results)
-        gc()
-        
-        
-        # run SAM
-        
-        data<-list(x=as.matrix(df.temp), y=sample_info$group
-                   , geneid=feature_info$protein_id,
-                   genenames=feature_info$protein_id, logged2=TRUE)
-        
-        samr.obj <- samr( data, resp.type="Two class unpaired", nperms=100)
-        logFC<-log2(samr.obj$foldchange)
-        
-        pvalue <- data.frame(samr.pvalues.from.perms(samr.obj$tt, samr.obj$ttstar))
-        
-        adj.pvalue <- p.adjust(pvalue$samr.pvalues.from.perms.samr.obj.tt..samr.obj.ttstar., method = "BH")
-
-        SAM.results<-cbind(logFC, pvalue, adj.pvalue)
-        
-        saveRDS(SAM.results, paste0("spectronaut_resutls/", "SAM_" , i.exp , "_" , i.Contrasts, ".rds"))
-        remove(SAM.results)
-        gc()
-        
-        
-        # run t-test
-        i.test.result <- data.frame()
-        for(i.test in seq_len(nrow(df.temp))){
-            i.test.df <- df.temp[i.test,]
-            pvalue <- t.test(formula = as.numeric(i.test.df[1,])~sample_info$group)$p.value
-            fc.calc <- mean( as.numeric( i.test.df[,1:select.col.Contrasts1] ) ) - mean( as.numeric(i.test.df[,select.col.Contrasts1+1:select.col.Contrasts2]) )
-            
-            i.test.result <- rbind(i.test.result, data.frame(row.names = row.names(i.test.df), 
-                                                             pvalue = pvalue , 
-                                                             fc.calc = fc.calc))
-        }
-        
-        i.test.result$adj.pvalue <- p.adjust(i.test.result$pvalue, method = "BH")
-        
-        
-        saveRDS(i.test.result, paste0("spectronaut_resutls/", "ttest_" , i.exp , "_" , i.Contrasts, ".rds"))
-        remove(i.test.result)
-        gc()
-        
-        # run ANOVA
-        
-        
-        ## codes from:
-        #https://www.bioconductor.org/packages/release/bioc/vignettes/DEqMS/inst/doc/DEqMS-package-vignette.html#compare-p-values-from-deqms-to-ordinary-t-test-anova-and-limma
-        design.model = model.matrix(~0+group , data = sample_info) 
-        
-        fit1 = lmFit(df.temp,
-                     design = design.model)
-        ord.t = fit1$coefficients[, 1]/fit1$sigma/fit1$stdev.unscaled[, 1]
-        ord.p = 2*pt(abs(ord.t), fit1$df.residual, lower.tail = FALSE)
-        ord.q = p.adjust(ord.p,method = "BH")
-        log2FC = fit1$coefficients[,2]-fit1$coefficients[,1]
-        anova.results = data.frame(row.names(df.temp),
-                                   logFC=log2FC,
-                                   t=ord.t, 
-                                   P.Value=ord.p, 
-                                   adj.P.Val = ord.q)
-        saveRDS(anova.results, paste0("spectronaut_resutls/", "ANOVA_" , i.exp , "_" , i.Contrasts, ".rds"))
-        remove(anova.results)
-        gc()
-        
-        
-        }
-    }
-}
-
