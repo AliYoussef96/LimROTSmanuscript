5f4c335a830c68ed18b86684573ded738339d0a6
Ali Youssef <aliali.mostafa99@gmail.com>
2025-06-24 20:54:12 +0300

    Update HEof_Spectronaut.r

--- DIA/Case study 3/HEof_Spectronaut.r
+++ DIA/Case study 3/HEof_Spectronaut.r
@@ -95,14 +95,11 @@ for(i.Contrasts in Contrasts){
         df.temp <- df.temp[,design.temp$sample_name]
         
         # Impute missing data using impSeq method
-        if(any(is.na(df.temp))){
-            df.temp <-  impSeq(df.temp)
-            df.temp <- data.frame(df.temp, check.names = F, check.rows = F)
-        }else{
-            df.temp <-  impSeq(df.temp)
-            df.temp <- data.frame(df.temp, check.names = F, check.rows = F)
-        }
-        
+
+        df.temp <-  impSeq(df.temp)
+        df.temp <- data.frame(df.temp, check.names = F, check.rows = F)
+
+    
         # Add random effect to a subset of ECOLI proteins in 'W' batch
         add.effect <- design.temp[design.temp$batch == "W" & design.temp$condition == design.temp$condition[1],]
         ecoli <- sample(which(grepl("ECOLI", row.names(df.temp))), 500)
